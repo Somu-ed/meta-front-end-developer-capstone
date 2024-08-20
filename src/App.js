2b104bc4ae60a0667cad493e9b6fbbabcf4d3e53
Soumyajeet Satpathy <satpathysoumyajeet.99@gmail.com>
2024-08-20 20:48:18 +0530

    Layout complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import './App.css';
+import {
+  Route, 
+  Routes
+} from 'react-router-dom';
+import pages from './Components/Layout/utils/pages';
+import Layout from './Components/Layout/Layout';
+import Home from './Components/Pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <div>Homepage</div>
-    </div>
+    <>
+      <Layout>
+        <Routes>
+          <Route path={pages.get('home').path} element={<Home />} />
+          {/* <Route 
+            path={pages.get('about').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route 
+            path={pages.get('menu').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route path={pages.get('bookings').path} element={<Bookings />} />
+          <Route 
+            path={pages.get('confirmedBooking').path} 
+            element={<ConfirmedBooking />} 
+          />
+          <Route 
+            path={pages.get('orderOnline').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route 
+            path={pages.get('login').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </Layout>
+    </>
   );
 }
 
